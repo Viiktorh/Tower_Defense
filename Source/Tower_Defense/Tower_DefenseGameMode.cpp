51cd0c9ee28d1de201a7d2a900bdf1d90d9313f5
Anda <andzelikawos56@gmail.com>
2024-09-25 15:28:11 +0200

    enemy spawn added
    added random location enemy spawn every 2sec

--- Source/Tower_Defense/Tower_DefenseGameMode.cpp
+++ Source/Tower_Defense/Tower_DefenseGameMode.cpp
@@ -3,6 +3,8 @@
 #include "Tower_DefenseGameMode.h"
 #include "Tower_DefenseCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "EnemySpawn.h"
 
 ATower_DefenseGameMode::ATower_DefenseGameMode()
 {
@@ -12,4 +14,42 @@ ATower_DefenseGameMode::ATower_DefenseGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATower_DefenseGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	EnemyTimer -= DeltaTime;
+
+	if (EnemyTimer < 0.0f)
+	{
+		EnemyTimer = 2.0f;
+
+		UWorld* world = GetWorld();
+		
+		if (world)
+		{
+			int playerIndex = 0;
+
+			FVector playerLocation = UGameplayStatics::GetPlayerCharacter(GetWorld(), playerIndex)->GetActorLocation();
+
+			FVector enemyLocation = playerLocation;
+
+			float randomDistance = FMath::RandRange(100.0f, 1000.0f);
+
+			enemyLocation.X += randomDistance;
+			enemyLocation.Y += randomDistance;
+
+			AEnemySpawn* enemy = world->SpawnActor<AEnemySpawn>(EnemyBlueprint, enemyLocation, FRotator::ZeroRotator);
+		}
+	}
+
+}
+
+void ATower_DefenseGameMode::BeginPlay()
+{
+	Super::BeginPlay();
 }
