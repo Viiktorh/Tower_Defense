bfec7286f74f70a638ab5a378e5c4d8ad397bc88
RomHarel555 <143715923+RomHarel555@users.noreply.github.com>
2024-11-13 15:15:14 +0100

    tower placement and free cursor
    made player character be able to place and undo towers. you place towers by pressing E, and undo towers by pressing G. when holding E you get a free cursor to place your towers and the tower mesh is set to false as to not mess with other object and the player view. mesh becomes true again when action is released

--- Source/Tower_Defense/Tower_DefenseCharacter.cpp
+++ Source/Tower_Defense/Tower_DefenseCharacter.cpp
@@ -1,175 +1,142 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "Tower_DefenseCharacter.h"
-#include "Engine/LocalPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
-#include "GameFramework/Controller.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include "InputActionValue.h"
-#include "DrawDebugHelpers.h"
-#include "Enemy.h"
-
-
-//////////////////////////////////////////////////////////////////////////
-// ATower_DefenseCharacter
+#include "GameFramework/Controller.h"
+#include "Engine/World.h"
+#include "Tower.h"
 
 ATower_DefenseCharacter::ATower_DefenseCharacter()
 {
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+    GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
-	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
-	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationYaw = false;
+    bUseControllerRotationRoll = false;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+    GetCharacterMovement()->JumpZVelocity = 700.f;
+    GetCharacterMovement()->AirControl = 0.35f;
+    GetCharacterMovement()->MaxWalkSpeed = 500.f;
 
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 400.0f;
+    CameraBoom->bUsePawnControlRotation = false;
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
+
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATower_DefenseCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (PreviewTower)
+    {
+        APlayerController* PlayerController = Cast<APlayerController>(GetController());
+        if (PlayerController)
+        {
+            FHitResult HitResult;
+            if (PlayerController->GetHitResultUnderCursorByChannel(UEngineTypes::ConvertToTraceType(ECollisionChannel::ECC_Visibility), true, HitResult))
+            {
+                FVector SpawnLocation = HitResult.Location;
+                PreviewTower->SetActorLocation(SpawnLocation);
+            }
+        }
+    }
 }
 
 void ATower_DefenseCharacter::BeginPlay()
 {
-	// Call the base class  
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
-
-//////////////////////////////////////////////////////////////////////////
-// Input
 
 void ATower_DefenseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	// Add Input Mapping Context
-	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
-			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		}
-	}
-	
-	// Set up action bindings
-	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
-		
-		// Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+    if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
+    }
 
-		// Moving
-		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATower_DefenseCharacter::Move);
-
-		// Looking
-		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATower_DefenseCharacter::Look);
-
-		// Shooting
-		EnhancedInputComponent->BindAction(PushAction, ETriggerEvent::Started, this, &ATower_DefenseCharacter::PushEnemy);
-		
-	}
-
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATower_DefenseCharacter::Move);
+        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ATower_DefenseCharacter::Look);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+        EnhancedInputComponent->BindAction(IA_PlaceTowerAction, ETriggerEvent::Started, this, &ATower_DefenseCharacter::PlaceTower);
+        EnhancedInputComponent->BindAction(IA_PlaceTowerAction, ETriggerEvent::Completed, this, &ATower_DefenseCharacter::FinalizeTowerPlacement);
+        EnhancedInputComponent->BindAction(IA_UndoTowerAction, ETriggerEvent::Started, this, &ATower_DefenseCharacter::UndoTowerPlacement);
+    }
 }
 
-void ATower_DefenseCharacter::Move(const FInputActionValue& Value)
+void ATower_DefenseCharacter::PlaceTower()
 {
-	// input is a Vector2D
-	FVector2D MovementVector = Value.Get<FVector2D>();
+    if (!TowerBlueprint) return;
 
-	if (Controller != nullptr)
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+    APlayerController* PlayerController = Cast<APlayerController>(GetController());
+    if (!PlayerController) return;
 
-		// get forward vector
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
-		// get right vector 
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+    FVector CameraLocation;
+    FRotator CameraRotation;
+    PlayerController->GetPlayerViewPoint(CameraLocation, CameraRotation);
+    FVector SpawnLocation = CameraLocation + CameraRotation.Vector() * 100.0f;
 
-		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
-	}
+    PreviewTower = GetWorld()->SpawnActor<ATower>(TowerBlueprint, SpawnLocation, FRotator::ZeroRotator);
+
+    if (PreviewTower)
+    {
+        PreviewTower->SetActorEnableCollision(false);
+        PreviewTower->SetActorTickEnabled(false);
+
+        if (APlayerController* PC = Cast<APlayerController>(GetController()))
+        {
+            PC->bShowMouseCursor = true;
+            PC->bEnableClickEvents = true;
+            PC->bEnableMouseOverEvents = true;
+        }
+    }
 }
 
-void ATower_DefenseCharacter::Look(const FInputActionValue& Value)
+void ATower_DefenseCharacter::FinalizeTowerPlacement()
+//**we are aware you can place the tower on yourself, we do not have valid locations checks currently**//
 {
-	// input is a Vector2D
-	FVector2D LookAxisVector = Value.Get<FVector2D>();
+    if (PreviewTower)
+    {
+        PreviewTower->SetActorEnableCollision(true);
+        PreviewTower->SetActorTickEnabled(true);
+        PlacedTowers.Add(PreviewTower);
+        PreviewTower = nullptr;
 
-	if (Controller != nullptr)
-	{
-		// add yaw and pitch input to controller
-		AddControllerYawInput(LookAxisVector.X);
-		AddControllerPitchInput(LookAxisVector.Y);
-	}
+        if (APlayerController* PC = Cast<APlayerController>(GetController()))
+        {
+            PC->bShowMouseCursor = false;
+            PC->bEnableClickEvents = false;
+            PC->bEnableMouseOverEvents = false;
+        }
+    }
 }
 
-void ATower_DefenseCharacter::PushEnemy()
+void ATower_DefenseCharacter::UndoTowerPlacement()
 {
-	FHitResult OutHit;
-
-	AController* PlayerController = GetController();
-	//Determines where the Beam start and end locations are.
-	FRotator BeamRotation = FollowCamera->GetRelativeRotation();
-	FVector BeamStart = GetActorLocation();
-	FVector ForwardVector = FollowCamera->GetForwardVector();
-	FVector BeamEnd = ((ForwardVector * 1800.f) + BeamStart);
-	FVector PushBackDirection = BeamStart - (BeamStart.Z - 50.f);
-	
-	//Initialize Collision parameters
-	FCollisionQueryParams CollisionParams;
-
-	DrawDebugLine(GetWorld(), BeamStart, BeamEnd, FColor::Red, false, 3.0f, 0, 1);
-
-	//Execute a LineTrace with OutHit as result
-	GetWorld()->LineTraceSingleByChannel(OutHit, BeamStart, BeamEnd, TraceChannelProperty, CollisionParams);
-
-	// If the trace hit something, bBlockingHit will be true,
-	// and its fields will be filled with detailed info about what was hit
-	if (OutHit.bBlockingHit && IsValid(OutHit.GetActor()))
-	{
-		if(OutHit.GetActor() != nullptr)
-		{
-			//TODO: This does not push enemies away, it only teleports them up.
-			FVector VectorToEnemy = GetActorLocation() + (OutHit.GetActor()->GetActorLocation() - GetActorLocation());
-			FVector PushBackEndPoint = VectorToEnemy + (GetActorLocation().ZAxisVector * 200.f);
-			OutHit.GetActor()->SetActorLocation(PushBackEndPoint);
-			//OutHit.GetComponent()->AddImpulse(ForwardVector*1000,FName("NAME_None"), true);
-			/*UPrimitiveComponent* component = Cast<UPrimitiveComponent>(OutHit.GetActor()->GetRootComponent());
-			if (component != nullptr)
-			{
-				component->AddImpulseAtLocation(ForwardVector*500, BeamEnd);
-			}
-			OutHit.GetActor()->GetRootComponent()->AddLocalRotation(BeamRotation);*/
-			//SetActorLocation(GetActorLocation() + (Direction * MoveSpeed * GetWorld()->GetDeltaSeconds()));
-			DrawDebugLine(GetWorld(), BeamStart, PushBackEndPoint, FColor::Cyan, false, 2.0f, 0, 1);
-		}
-	}
+    if (PlacedTowers.Num() > 0)
+    {
+        ATower* LastTower = PlacedTowers.Pop();
+        if (LastTower)
+        {
+            LastTower->Destroy();
+        }
+    }
 }
--- Source/Tower_Defense/Tower_DefenseCharacter.h
+++ Source/Tower_Defense/Tower_DefenseCharacter.h
@@ -1,86 +1,95 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Logging/LogMacros.h"
 #include "Tower_DefenseCharacter.generated.h"
 
 class AEnemy;
+class ATower;
 class USpringArmComponent;
 class UCameraComponent;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
 
-UCLASS(config=Game)
+UCLASS(config = Game)
 class ATower_DefenseCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	USpringArmComponent* CameraBoom;
+    /** Camera boom positioning the camera behind the character */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	UCameraComponent* FollowCamera;
-	
-	/** MappingContext */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputMappingContext* DefaultMappingContext;
+    /** Follow camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    UCameraComponent* FollowCamera;
 
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* JumpAction;
+    /** MappingContext */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputMappingContext* DefaultMappingContext;
 
-	/** Move Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* MoveAction;
+    /** Jump Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* JumpAction;
 
-	/** Look Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* LookAction;
+    /** Move Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* MoveAction;
 
-	/** Fire weapon Action*/
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	class UInputAction* PushAction;
+    /** Look Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* LookAction;
 
-	/** Fire weapon Function*/
-	UFUNCTION(BlueprintCallable, Category = "Weapon function")
-	void PushEnemy();
+    /** Fire weapon Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* PushAction;
 
-public:
-	ATower_DefenseCharacter();
-	
-
-	/*
-	 * Expose Collision Channel to Unreal Editor
-	 */
-	UPROPERTY(EditAnywhere, Category = "Beam Collision")
-	TEnumAsByte<ECollisionChannel> TraceChannelProperty = ECC_Visibility;
-
-protected:
-
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-			
-
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-	// To add mapping context
-	virtual void BeginPlay();
+    /** Fire weapon Function */
+    UFUNCTION(BlueprintCallable, Category = "Weapon function")
+    void PushEnemy();
 
 public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+    ATower_DefenseCharacter();
+
+    /** Preview tower */
+    ATower* PreviewTower;
+
+    virtual void Tick(float DeltaSeconds) override;
+
+    /** Tower Placement Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* IA_PlaceTowerAction;
+
+    /** Undo Tower Placement Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* IA_UndoTowerAction;
+
+    /** Tower Blueprint to spawn */
+    UPROPERTY(EditAnywhere, Category = "Tower")
+    TSubclassOf<ATower> TowerBlueprint;
+
+    
+protected:
+    /** Called for movement input */
+    void Move(const FInputActionValue& Value);
+
+    /** Called for looking input */
+    void Look(const FInputActionValue& Value);
+
+    /** Called for placing a tower */
+    void PlaceTower();
+
+    /** Called for finalizing tower placement */
+    void FinalizeTowerPlacement();
+
+	/** Called for undoing the last tower placement */
+    void UndoTowerPlacement();
+
+    /** Stack to store placed towers */
+    TArray<ATower*> PlacedTowers;
+
+protected:
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    virtual void BeginPlay();
 };
-
